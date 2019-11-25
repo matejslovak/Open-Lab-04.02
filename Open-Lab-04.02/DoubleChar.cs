b9176d6dccd00b1d5a5b0ec43980103cfc1435bc
matejslovak <joggurtik1@gmail.com>
2019-11-25 09:48:43 +0100

    done

---
+++ Open-Lab-04.02/DoubleChar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_04._02
+{
+    class DoubleChar
+    {
+        public void GetDoubleChar(string str)
+        {
+            string[] arr = new string[str.Length];
+            for (int a = 0; a < str.Length; a++)
+            {
+                string array = arr[a] = str[a].ToString() + str[a].ToString();
+                Console.Write(array);
+            }
+        }
+    }
+
+}
---
+++ Open-Lab-04.02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_04._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleChar doublechar = new DoubleChar();
+            doublechar.GetDoubleChar(Console.ReadLine());
+        }
+    }
+}
